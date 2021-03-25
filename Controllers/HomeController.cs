f2b1af87fc5585e0eb8f75bc0fc3ebd0338696a8
RaduLepadatu <radu.lepadatu@principal33.com>
2021-03-25 13:05:05 +0200

    working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,27 +11,18 @@ namespace RaduMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly InternshipClass _internshipClass;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
+            _internshipClass = new InternshipClass();
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(_internshipClass);
         }
     }
 }
