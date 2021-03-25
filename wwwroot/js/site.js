c781312f86adf644540c3a3cfcfd3e4650cc252a
RaduLepadatu <radu.lepadatu@principal33.com>
2021-03-25 12:02:40 +0200

    Replace entire codebase

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.getElementById("addButton").onclick = function () {
+        var node = document.createElement("li");
+        node.innerText = newcomer.value;
+        list.appendChild(node);
 
-// Write your JavaScript code.
-document.getElementById("add").onclick = function () {
-    var node = document.createElement("li");
-    node.innerText = newcomer.value;
-    list.appendChild(node);
 };
