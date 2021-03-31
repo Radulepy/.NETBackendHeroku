5a2ca3d475a9d7bed5da55cc206a806b6ea695d5
RaduLepadatu <radu.lepadatu@principal33.com>
2021-03-31 10:53:23 +0300

    added Swagger documentation

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,10 +29,14 @@ namespace RaduMVC
         {
             services.AddControllersWithViews();
             services.AddSingleton(typeof(InternshipService));
-		  services.AddSwaggerGen(c =>
-		            {
-		                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipMVC.WebAPI", Version = "v1" });
-		            });
+		    services.AddSwaggerGen(c =>
+		        {
+		            c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipMVC.WebAPI", Version = "v1" });
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
