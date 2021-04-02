d49350bb953517a7de1747a18d7d1942062b4f97
RaduLepadatu <radu.lepadatu@principal33.com>
2021-04-02 12:51:28 +0300

    added Db context

---
+++ Data/InternDbContext.cs
@@ -0,0 +1,22 @@
+﻿using RaduMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaduMVC.Data
+{
+    public class InternDbContext : InternDbContext
+    {
+        public InternDbContext(DbContextOptions<InternDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Intern> Interns { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Intern>().ToTable("Intern");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RaduMVC.Data;
 using RaduMVC.Services;
 
 namespace RaduMVC
@@ -27,16 +29,23 @@ namespace RaduMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<InternDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddControllersWithViews();
             services.AddSingleton(typeof(InternshipService));
-		    services.AddSwaggerGen(c =>
-		        {
-		            c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipMVC.WebAPI", Version = "v1" });
-                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                    c.IncludeXmlComments(xmlPath);
-                });
-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RazorMVC.WebAPI", Version = "v1" });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
